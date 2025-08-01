7b079dac14bb507be8eecc42014ded89871c635a
honoka5 <s2370179@s.do-johodai.ac.jp>
2025-08-02 05:27:46 +0900

    final prefs = await SharedPreferences.getInstance();アプリを閉じてもデータが残る。await prefs.setString('last_opened', DateTime.now().toIso8601String());は現在の日時を取得する。6_nitice_button.dartで読み取る為インポートしました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -8,5 +9,13 @@ Future<void> main() async {
     url: 'https://xcogckezaetipkwsnahp.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhjb2dja2V6YWV0aXBrd3NuYWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NDk3NDMsImV4cCI6MjA2NjUyNTc0M30.qAJktQrpAwGqVOZriaRwjWh-HKkCV2x8nUqso6w7LfA',
   );
+  // アプリ起動時刻を記録
+  await _recordAppOpen();
+
   runApp(MyApp());
+}
+Future<void> _recordAppOpen() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('last_opened', DateTime.now().toIso8601String());
+ 
 }
