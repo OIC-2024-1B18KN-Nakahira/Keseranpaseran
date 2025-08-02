67e2077b84f9b2fe74f7e37a57b9572794eb8936
honoka5 <s2370179@s.do-johodai.ac.jp>
2025-08-03 05:10:40 +0900

    依存ライブラリの取得元を指定 ビルド成果物の出力先をカスタマイズ クリーン処理を定義しています

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,16 +5,10 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(file("../build"))
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
+    project.layout.buildDirectory.set(File(rootProject.layout.buildDirectory.get().asFile, project.name))
 }
 
 tasks.register<Delete>("clean") {
